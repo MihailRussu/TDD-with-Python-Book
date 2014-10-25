c1670bc68d9c10a3e543d538b1ebc41f8b3c60fb
Mihail Russu <mihail@russu.net>
2014-10-24 19:59:12 -0500

    Add comments and placeholders for future tests

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,27 @@
 from selenium import webdriver
 
+# initialize the browser object
 browser = webdriver.Firefox()
+
+# open the browser with our ptoject
 browser.get('http://localhost:8000')
 
+# see if the title contains "Django" 
 assert 'Django' in browser.title
+
+# see of the page contains input element
+
+# try to type something in that input
+
+# try to hit "Enter" after typing and see if what was typed showed up
+
+# try to add another item
+
+# see if the page now has both items
+
+# try to navigate to one item's url
+
+# see if the navigated url contains everything
+
+# quit the browser
+browser.quit()
